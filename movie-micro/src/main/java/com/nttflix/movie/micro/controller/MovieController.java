cfd8c5a80ae536ef16863a9fcde961f62688414f
KevinObandoMorales <61804384+KevinObandoMorales@users.noreply.github.com>
2022-11-22 13:38:17 +0100

    Add files via upload

--- movie-micro/src/main/java/com/nttflix/movie/micro/controller/MovieController.java
+++ movie-micro/src/main/java/com/nttflix/movie/micro/controller/MovieController.java
@@ -1,5 +1,8 @@
 package com.nttflix.movie.micro.controller;
 
-public class MovieController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class MovieController {
+	
 }
---
+++ movie-micro/src/test/java/com/nttflix/movie/micro/MovieMicroApplicationTests.java
@@ -0,0 +1,18 @@
+package com.nttflix.movie.micro;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nttflix.movie.micro.controller.MovieController;
+import com.nttflix.movie.micro.service.MovieService;
+
+@SpringBootTest
+class MovieMicroApplicationTests {
+
+    @Test
+    public void contextLoads() {}
+    
+}
