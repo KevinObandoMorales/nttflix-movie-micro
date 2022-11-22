eaf54bacc56b78cbce3ef63f85f93ec17b2703a6
KevinObandoMorales <61804384+KevinObandoMorales@users.noreply.github.com>
2022-11-22 11:03:19 +0100

    Add files via upload

---
+++ movie-micro/src/test/java/com/netflix/movie/micro/MovieMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.movie.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
