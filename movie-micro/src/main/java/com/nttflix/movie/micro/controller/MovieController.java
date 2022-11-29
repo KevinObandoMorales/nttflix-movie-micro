0ceffb2b3e571977a74b832eacac04db444ddcca
KevinObandoMorales <kevin.a.obando@gmail.com>
2022-11-29 11:00:12 +0100

    micro movies v1.0

--- movie-micro/src/main/java/com/nttflix/movie/micro/controller/MovieController.java
+++ movie-micro/src/main/java/com/nttflix/movie/micro/controller/MovieController.java
@@ -18,61 +18,60 @@ import com.nttflix.movie.micro.service.MovieService;
 
 @RestController
 public class MovieController {
-	
+
 	@Autowired
 	private MovieService movieService;
-	
 
 	@GetMapping("/api/movies")
-    public ResponseMovie getMovies(){	
+	public ResponseMovie getMovies() {
 		List<MovieData> listMovies = movieService.getAllMovies();
-		String msg = "Movies found";
+		String msg = (listMovies.isEmpty()) ? "Movies not found" : "Movies found";
+		
+		return new ResponseMovie(msg,listMovies);
+	}
+
+	@GetMapping("/api/movies/number")
+	public ResponseMovie getMovie(@RequestBody RequestMovie requestScreen) {
+		String macNumber = requestScreen.getMovieData().getMac();
+		List<MovieData> listMovies = movieService.getMoviesByMAC(macNumber);
+		String msg = (listMovies.isEmpty()) ? "Movies not found" : "Movies by MAC: " + macNumber;
+		
 		return new ResponseMovie(msg, listMovies);
-   }
-    
-    @GetMapping("/api/movies/number")
-    public ResponseMovie getMovie(@RequestBody RequestMovie requestScreen){
-    	String macNumber = requestScreen.getMovieData().getMac();
-    	List<MovieData> listMovies = movieService.getMoviesByMAC(macNumber);
-    	String msg = "Movies by MAC: " + macNumber;
-    	
-        return new ResponseMovie(msg, listMovies);
-    }
-    
-    
-    @PostMapping("/api/movies")
-    public ResponseMovie addMovies(@RequestBody RequestMovie requestScreen){
-    	MovieData movieDataNew = movieService.createMovie(requestScreen.getMovieData());
-    	String msg = "Se ha añadido correctamente";
-    	return new ResponseMovie(msg, movieDataNew);
-   }
-    
-    
-    @DeleteMapping("/api/movies")
-    public ResponseMovie deleteMovies(@RequestBody RequestMovie requestScreen){	
-    	Long id = requestScreen.getMovieData().getId();
-    	Optional<MovieData> movieData = movieService.getMovieById(id);
-    	String msg = "Not found";
-        
-    	if (movieData.isPresent()) {
-        	msg = "Movie deleted";
-        	movieService.deleteMovie(id);
-        	return new ResponseMovie(msg, movieData.get());
-        }
-        return new ResponseMovie(msg, null);
-   }
-    
-    @PutMapping("/api/movies")
-    public ResponseMovie editMovies(@RequestBody RequestMovie requestScreen){
-    	Long id = requestScreen.getMovieData().getId();
-    	Optional<MovieData> movieData = movieService.getMovieById(id);
-    	String msg = "Movie not found";
-    	
-    	if (movieData.isPresent()) {
-    		msg = "Movie updated";
-    		MovieData movieUpdated = movieService.updateMovie(requestScreen.getMovieData());
-    		return new ResponseMovie(msg, movieUpdated);
-    	}
-    	return new ResponseMovie(msg, null);
-   }
+	}
+
+	@PostMapping("/api/movies")
+	public ResponseMovie addMovies(@RequestBody RequestMovie requestScreen) {
+		MovieData movieDataNew = movieService.createMovie(requestScreen.getMovieData());
+		String msg = "Se ha añadido correctamente";
+		
+		return new ResponseMovie(msg, movieDataNew);
+	}
+
+	@DeleteMapping("/api/movies")
+	public ResponseMovie deleteMovies(@RequestBody RequestMovie requestScreen) {
+		Long id = requestScreen.getMovieData().getId();
+		Optional<MovieData> movieData = movieService.getMovieById(id);
+		String msg = "Not found";
+
+		if (movieData.isPresent()) {
+			msg = "Movie deleted";
+			movieService.deleteMovie(id);
+			return new ResponseMovie(msg, movieData.get());
+		}
+		return new ResponseMovie(msg, null);
+	}
+
+	@PutMapping("/api/movies")
+	public ResponseMovie editMovies(@RequestBody RequestMovie requestScreen) {
+		Long id = requestScreen.getMovieData().getId();
+		Optional<MovieData> movieData = movieService.getMovieById(id);
+		String msg = "Movie not found";
+
+		if (movieData.isPresent()) {
+			msg = "Movie updated";
+			MovieData movieUpdated = movieService.updateMovie(requestScreen.getMovieData());
+			return new ResponseMovie(msg, movieUpdated);
+		}
+		return new ResponseMovie(msg, null);
+	}
 }
